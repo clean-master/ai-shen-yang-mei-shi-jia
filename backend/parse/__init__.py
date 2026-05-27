bea85a111b05d15abcac57baf4b9847dcb5de7c8
mccm <37928663+mccleaner@users.noreply.github.com>
2026-05-27 17:03:12 +0800

    删除旧版 backend、bili-bot 及 public 目录文件

--- backend/parse/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
- @Author: jiran
- @Email: jiran214@qq.com
- @FileName: __init__.py.py
- @DateTime: 2023/4/7 14:47
- @SoftWare: PyCharm
-"""
--- backend/requestor/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
- @Author: jiran
- @Email: jiran214@qq.com
- @FileName: __init__.py.py
- @DateTime: 2023/4/7 14:26
- @SoftWare: PyCharm
-"""
